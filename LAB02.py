5c1e52ec7e5f5367c8eff8326e62850f5ba1d00e
LongTruog <90683470+LongTruog@users.noreply.github.com>
2021-09-26 21:15:36 +0700

    Add Cau 01

---
+++ LAB02.py
@@ -0,0 +1,16 @@
+# Chương 8
+kingdoms = ['Bacteria', 'Protozoa', 'Chromista',
+'Plantaee', 'Fungi','Animaliaa']
+print("kingdoms: ", kingdoms)
+#1a
+print("kingdoms[0] : ", kingdoms[0])
+#1b
+print("kingdoms[5] : ", kingdoms[5])
+#1c
+print("kingdoms[:3]: ", kingdoms[:3])
+#1d
+print("kingdoms[2:5]: ", kingdoms[2:5])
+#1e 
+print("kingdoms[4:]: ", kingdoms[4:])
+#1f
+print("kingdoms[1:0]: ", kingdoms[1:0])
